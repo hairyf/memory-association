c87f561a850ccd75ad22e3a90d1ec6bc7e5915ab
Mr.Mao <951416545@qq.com>
2023-02-22 23:58:32 +0800

    feat: init commit and deploy

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const groups = [
+  ['1', 'do', 'C'],
+  ['2', 're', 'D'],
+  ['3', 'mi', 'E'],
+  ['4', 'fa', 'F'],
+  ['5', 'sol', 'G'],
+  ['6', 'la', 'A'],
+  ['7', 'xi', 'B'],
+]
+
+const codes: number[] = []
+
+function getRandomNumber(length: number) {
+  return Math.floor(Math.random() * length)
+}
+
+function getRandomGroup() {
+  let num = getRandomNumber(groups.length)
+  if (codes.length === groups.length)
+    codes.length = 0
+  while (codes.includes(num))
+    num = getRandomNumber(groups.length)
+  codes.push(num)
+  return groups[num]
+}
+
+function reset() {
+  show.value = false
+  group.value = getRandomGroup()
+}
+
+const target = ref(1)
+
+const group = ref(getRandomGroup())
+
+const show = ref(false)
+</script>
+
+<template>
+  <div class="h-screen w-full flex flex-col">
+    <div class="text-center pt-5 text-size-xl font-bold">Memory Association</div>
+    <div class="flex-1 flex flex-col pt-10">
+      <div class="text-center my-5 py-2 text-size-xl font-bold" :class="[target !== index && !show && 'opacity-0']"
+        v-for="(item, index) in group" :key="item">
+        {{ item }}
+      </div>
+      <div class="flex justify-center gap-5 pt-10">
+        <button class="w-20" @click="show = !show">{{ show ? 'hide' : 'show' }}</button>
+        <button class="w-20" @click="reset">next</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
